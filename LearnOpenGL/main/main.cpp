ed0ac81c36f5cbf1135f6dc5694a45faf1a34406
PanCake47 <qubaolszewski@gmail.com>
2025-07-20 15:51:10 +0200

    change commit

--- LearnOpenGL/main/main.cpp
+++ LearnOpenGL/main/main.cpp
@@ -38,6 +38,16 @@ int main()
 		return -1;
 	}
 
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f, // left
+		 0.5f, -0.5f, 0.0f, // right
+		 0.0f,  0.5f, 0.0f  // top
+	}
+
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+
+
 	// render loop
 	while (!glfwWindowShouldClose(window))
 	{
